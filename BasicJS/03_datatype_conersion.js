2ea38777d8c4c0403101803fb995b33045cb6623
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-06 09:05:47 +0000

    JS Datatype conversion

---
+++ BasicJS/03_datatype_conersion.js
@@ -0,0 +1,5 @@
+let score = null;
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
---
+++ BasicJS/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "fake@gmail.com";
+var accountPass = 123;
+accountName = "Canara";
+
+// accountId = 345;
+accountEmail = "real@gmail.com";
+
+// console.log(accountId)
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountName);
+console.table([accountId,accountEmail,accountPass])
