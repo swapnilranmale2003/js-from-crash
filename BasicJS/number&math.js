fcff79060259f1fe897430f6e7f2a87416115c55
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-06 11:43:58 +0000

    Math operation Learnt & Performed

--- BasicJS/number&math.js
+++ BasicJS/number&math.js
@@ -5,11 +5,28 @@ console.log(balance)
 
 console.log(balance.toString().length)
 
-console.log(balance.toFixed(3))   //decimal point
+// console.log(balance.toFixed(3))   //decimal point
 
 const otherNumber = 1234.899; 
-console.log(otherNumber.toPrecision(4)) //precision value
+// console.log(otherNumber.toPrecision(4)) //precision value
 
 const hundreds = 100000000;
-console.log(hundreds.toLocaleString('en-IN'))
+// console.log(hundreds.toLocaleString('en-IN'))
   
+// Math
+
+const number = -9;
+console.log(Math.abs(number))
+console.log(Math.floor(9.6))
+console.log(Math.ceil(9.6))
+
+console.log(Math.round(9.4))
+console.log(Math.round(9.5))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10))
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random () * ((max-min)+1))+min)
