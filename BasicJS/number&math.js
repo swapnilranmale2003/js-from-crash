1f3dd73ebb50b98b89aba3df347f1334d984ab1e
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-06 11:20:25 +0000

    Numbers!

---
+++ BasicJS/number&math.js
@@ -0,0 +1,15 @@
+//Numbers
+const balance = new Number(1000);
+console.log(typeof(balance))
+console.log(balance)
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(3))   //decimal point
+
+const otherNumber = 1234.899; 
+console.log(otherNumber.toPrecision(4)) //precision value
+
+const hundreds = 100000000;
+console.log(hundreds.toLocaleString('en-IN'))
+  
