8c9e77a6e3c40d6049ea0c15057e0ae8010794ce
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-06 09:15:24 +0000

    Add Heap Code

--- BasicJS/Heap&stack.js
+++ BasicJS/Heap&stack.js
@@ -1,3 +1,15 @@
 let myChannelName="xyz";
 let anotherName= myChannelName;
-console.log(anotherName)
+// console.log(anotherName)
+
+//Heap Memory
+const userOne ={
+    name : "qwerty",
+    email :"xyz@gmail.com"
+
+}
+const userTwo = userOne;
+userTwo.email= "swapnil@thinkitive.com";
+
+console.log(userOne.email)
+console.log(userTwo.email)
