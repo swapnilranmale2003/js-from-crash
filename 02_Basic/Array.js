14167d48b62fe94e40b9fdd4e4f878309b010ef3
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-06 13:30:39 +0000

    slice & splice

--- 02_Basic/Array.js
+++ 02_Basic/Array.js
@@ -4,10 +4,10 @@
 const myArray = [0,1,2,3,4];
 // console.log(myArray[1])
 myArray.push(5);
-console.log(myArray)
+// console.log(myArray)
 
 myArray.unshift(15);
-console.log(myArray)
+// console.log(myArray)
 
 // myArray.pop();
 // console.log(myArray)
@@ -15,11 +15,19 @@ console.log(myArray)
 // myArray.shift();
 // console.log(myArray)
 
-console.log(myArray.includes(2));
+// console.log(myArray.includes(2));
 
 
-const newArray = myArray.join();
+// const newArray = myArray.join();
+// console.log(myArray)
+// console.log(newArray)
+// console.log(typeof newArray)
+
+//slice & splice
+console.log("A", myArray)
+const myn1 = myArray.splice(1,3);
+console.log(myn1)
 console.log(myArray)
-console.log(newArray)
-console.log(typeof newArray)
-
+// console.log("B", myArray)
+// const myn2 = myArray.slice(1,3);
+// console.log(myn2)
