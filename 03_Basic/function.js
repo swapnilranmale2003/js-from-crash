3df3390eb70a63c7726f464c82cf58214f6d426e
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 12:02:15 +0000

    Parameter & arguments

--- 03_Basic/function.js
+++ 03_Basic/function.js
@@ -1,5 +1,33 @@
-const Hello = ()=>{
+const Hello = () => {
     console.log("Hello World");
 }
+// Hello();
 
-Hello();
+function Sunny(params) {
+    console.log("S");
+    console.log("U");
+    console.log("N");
+    console.log("Y");
+}
+Sunny();
+
+// const sayName = () => {
+//     console.log("S");
+//     console.log("U");
+//     console.log("N");
+//     console.log("Y");
+// }
+
+// sayName();
+
+
+function add(number1, number2)//parameter 
+ {
+   console.log(number1+number2)
+}
+add(1,2)//arguments
+
+function add(number1, number2) {
+    console.log(number1+number2)
+ }
+ add(1,"2")
