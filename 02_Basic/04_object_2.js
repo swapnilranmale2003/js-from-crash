d891a5f2a4f6f0edaa6150dbfeeeb3ba30e5858a
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 07:32:26 +0000

    Nested Object literals

--- 02_Basic/04_object_2.js
+++ 02_Basic/04_object_2.js
@@ -1,5 +1,17 @@
 const tinderUser = {};
 tinderUser.id = 1;
 tinderUser.name = "Smith";
+tinderUser.isLoggedIn = !false; 
+// console.log(tinderUser)
 
-console.log(tinderUser)
+const employee = {
+    email : "employee@gmail.com",
+    fullname:{
+        employeeName:{
+            firstName : "Sunny",
+            lastName :"Ranmale"
+        }
+    }
+}
+
+console.log(employee.fullname.employeeName)
