a18c44806e26a1ccbb258de4418713e01422926a
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 12:13:00 +0000

    //using variable //return

--- 03_Basic/function.js
+++ 03_Basic/function.js
@@ -22,12 +22,28 @@ Sunny();
 
 
 function add(number1, number2)//parameter 
- {
-   console.log(number1+number2)
+{
+    console.log(number1 + number2)
 }
-add(1,2)//arguments
+add(1, 2)//arguments
 
-function add(number1, number2) {
-    console.log(number1+number2)
+// function add(number1, number2) {
+//     console.log(number1+number2)
+//  }
+//  const res = add(1,2);
+//  console.log(res)
+
+// function add(number1, number2) {  //using variable
+//    let res = number1+number2;
+//    return res;
+// }
+// const res = add(1, 2);
+// console.log("result", res)
+
+
+function add(number1, number2) {  //using variable
+    return number1+number2;
  }
- add(1,"2")
+ const res = add(1, 2);
+ console.log("result", res)
+ 
