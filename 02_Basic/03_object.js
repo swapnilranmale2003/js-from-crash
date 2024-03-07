898776d04784061d28584e5a745b7431040008c7
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 07:00:46 +0000

    Symbol & object literal

--- 02_Basic/03_object.js
+++ 02_Basic/03_object.js
@@ -1,8 +1,17 @@
 //singletone ------> Object.create
 
 //object literals-----> key & value pair
+const mySym = Symbol("Key1");
+
 const jsUser = {
     name:"John",
-    sirname :"Dahl"
+    [mySym]:"myKey1",
+    "full name" :"Briendan eich",
+    sirname :"Dahl",
+    email:"example@gmail.com",
+    age: 21
 }
-console.log(jsUser.name)
+// console.log(jsUser["full name"])
+// console.log(jsUser["email"])
+
+console.log(typeof jsUser[mySym])
