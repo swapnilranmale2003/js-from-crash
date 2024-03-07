dc81bc7fea52f7a615949a1515b2999230624011
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 13:18:38 +0000

    Global & Local scope in js

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,25 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function getData(){
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// }
+// getData();
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+
+}
+console.log(a);
+console.log(b);
+console.log(c);
