c0f95174f43b5fc681347d9de6aa35626fc644b5
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 06:13:26 +0000

    Array_02 push, concat, spread operator learn & performed

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,18 @@
+console.log("Array Basics part two");
+
+const marvel_heros = ['thor', 'ironman', 'spiderman'];
+// console.log("Marvel Heros",marvel_heros);
+
+const dc_heros = ['superman', 'flash', 'batman'];
+// console.log("dc Heros",dc_heros);
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros)
+
+// const common = marvel_heros.concat(dc_heros) //using concat
+// console.log(common)
+
+// const all_new_heros =[...marvel_heros, ...dc_heros];  //join usig speread oper
+// console.log(all_new_heros);
+
+
