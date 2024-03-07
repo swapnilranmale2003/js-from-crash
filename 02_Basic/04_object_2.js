e54c62effde111791bddeb919252e60ed5b57a04
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 07:37:58 +0000

    Merged two or more object using spread operator

--- 02_Basic/04_object_2.js
+++ 02_Basic/04_object_2.js
@@ -13,5 +13,21 @@ const employee = {
         }
     }
 }
+// console.log(employee.fullname.employeeName)
+// console.log(employee.fullname1 ?.employeeName, "")
 
-console.log(employee.fullname.employeeName)
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"a",
+    4:"b"
+}
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
+
+
