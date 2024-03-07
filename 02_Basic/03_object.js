1e4758d4200644f1852da06f7d5782bc6197fda4
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-07 07:13:53 +0000

    Function Basic and this keyword

--- 02_Basic/03_object.js
+++ 02_Basic/03_object.js
@@ -16,6 +16,12 @@ const jsUser = {
 
 // console.log(typeof jsUser[mySym])
 jsUser.email = "sunny@chagpt.com";
-Object.freeze(jsUser)
+// Object.freeze(jsUser)
 jsUser.email = "sunny@microsoft.com";
-console.log(jsUser)
+// console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log(`Hello js User ${this.age}`)  // this use when we refer the values in same Object.
+}
+jsUser.greeting();
+// console.log(jsUser.greeting())
