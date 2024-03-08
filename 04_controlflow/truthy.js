8482593d6b60b7ebeca18f844f8028123071d23d
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 09:40:57 +0000

    nullish coalescing operator

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -17,3 +17,13 @@ const emptyObj = {};
 if(Object.keys(emptyObj).length===0){
     console.log("empty");
 }
+
+//nullish coalescing operator ?? -->null undefined
+
+let val1
+// val = 5 ?? 10
+// val = null ?? 10
+// val = undefined ?? 10
+val = null ?? 10 ?? 20
+
+console.log(val);
