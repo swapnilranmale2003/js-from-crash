fef18e90797c132a924a60682cd6117e519eaf45
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 11:26:23 +0000

    forin loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,12 @@
+const obj ={
+    js:"javascript",
+    cpp:"c++"
+}
+for (const key in obj) {
+    // console.log(key)
+}
+
+const programming = ["js","cpp","py"];
+for (const p in programming) {
+    console.log(programming[p])
+}
