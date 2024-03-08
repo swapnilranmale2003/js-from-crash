9dea90eb213360939c9e04774b9228fdc38f422f
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 12:45:57 +0000

    filter with object

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -13,9 +13,41 @@ const programming = ["js","cpp","py"];
 //filter
 
 const myNum = [1,2,3,4,5,6,7,8,9,10]
-console.log(myNum.filter((num)=>num>4))
+// console.log(myNum.filter((num)=>num>4))
 
+
+// const myNum2 = [1,2,3,4,5,6,7,8,9,10]
+// const res = myNum2.filter((num1)=>num1<7)
+// console.log(res)
 
 const myNum2 = [1,2,3,4,5,6,7,8,9,10]
-const res = myNum2.filter((num1)=>num1<7)
-console.log(res)
+const res = myNum2.filter((num1)=>{
+    return num1<7
+})
+// console.log(res)
+
+const books = [
+    {
+        title:"C",
+        genre:"Programming",
+        publish:1972
+    },
+    {
+        title:"Cpp",
+        genre:"Programming",
+        publish:1979
+    },
+    {
+        title:"JS",
+        genre:"Scripting",
+        publish:1995
+    },
+    
+]
+
+// console.log(books)
+
+const userBooks = books.filter((bk)=>{
+    return bk.genre==='Programming';
+})
+console.log(userBooks)
