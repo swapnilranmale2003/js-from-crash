75d310db31735755a26ce19043768845c1fcaca6
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 13:51:50 +0000

    reduce method()

---
+++ 05_iteration/eight.js
@@ -0,0 +1,15 @@
+//reduce
+// const array1 = [1, 2, 3, 4];
+// const total = 0;
+
+// const sum = array1.reduce(
+//     (a, b) => a + b,
+//     total,
+// );
+
+// console.log(sum);
+
+const numbers = [10,20,30];
+const initialValue=0;
+const sum = numbers.reduce((ac, currentvalue)=>ac+currentvalue,initialValue)
+console.log(sum)
