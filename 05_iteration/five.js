8d39a829e1a0d96b04aa69f0328ca2d55e56ff99
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 12:29:46 +0000

    iteration

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,4 +1,12 @@
 const coding = ["java","php", "cpp"];
-coding.forEach (function(sub){
-    console.log(sub);
+// coding.forEach (function(sub){
+//     console.log(sub);
+// })
+
+coding.forEach((item)=>{
+    // console.log(item)
 })
+
+function printMe(coding){
+    console.log(coding)
+}
