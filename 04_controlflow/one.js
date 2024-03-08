88c582b8e3fd32f65471042d7cbe162e6158c0e1
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 08:41:55 +0000

    shorthand notation

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -1,8 +1,29 @@
-const temprature = 10;
-if(temprature>40)
-{
-    console.log("temprature is greater han 50");
+// const temprature = 10;
+// if(temprature>40)
+// {
+//     console.log("temprature is greater han 50");
+// }
+// else{
+// console.log("temprature is less than 50")
+// }
+
+// <, >, <=, >=, ==, ===, !=, !==
+/*
+const score = 200;
+if(score>100){
+    var power = "fly";   //var is global scope 
+    console.log(`power fly:${power} `);
 }
-else{
-console.log("temprature is less than 50")
+console.log(`power fly:${power} `);
+*/
+const score = 200;
+if(score>100){
+    let power = "fly";   //var is global scope 
+    // console.log(`power fly:${power} `);
 }
+// console.log(`power fly:${power} `);
+
+//shorthand notation
+
+const balance = 1000;
+if(balance>500) console.log("test")
