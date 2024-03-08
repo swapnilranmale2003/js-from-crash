0b8d6a342ccc9d368ed958f38bd6040946168c64
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 10:27:11 +0000

    while & do-while loop!

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+// let i=1;
+// while (i<10) {
+//     console.log(i)
+//     i++
+// }
+
+const myArray = ['A','B','C'];
+let i=0;
+while(i<myArray.length){
+    // console.log(myArray[i])
+    i++
+}
+
+//do while
+let score=11;
+do {
+    console.log(`score is ${score}`);
+    score = score+1
+} while (score<=10);
