0d1214753796a8f30f009ad99d6079b73bd16b32
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 09:10:13 +0000

    switch case in js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,46 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 1;
+// switch (month) {
+//     case 1:
+//         console.log("JAN");
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "feb";
+switch (month) {
+    case "jan":
+        console.log("1");
+        break;
+    case "feb":
+        console.log("2");
+        break;
+    case "march":
+        console.log("3");
+        break;
+    case "april":
+        console.log("4");
+        break;
+    case "may":
+        console.log("5");
+        break;
+    case "june":
+        console.log("6");
+        break;
+    case "july":
+        console.log("7");
+        break;
+    default:
+        break;
+}
