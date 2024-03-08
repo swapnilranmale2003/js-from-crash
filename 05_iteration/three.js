5fa8aa03ffc5f4d8a615d8447da6b25c66627df7
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-08 11:00:37 +0000

    forof loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,14 @@
+// const arr = [1,2,3,5,6];
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+const greeting = "Hello World";
+
+for (const greet of greeting){
+    if(greet==" "){
+    continue;
+    }
+    console.log(`Each char is ${greet}`)
+
+}
