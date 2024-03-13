a39ba4ca2281f6e358b7aab8f7b8c467f54ddd47
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-13 08:39:07 +0000

    map

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -1,5 +1,9 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = numbers.map((num) =>{return num * 10;})
-                        .filter((num)=>num<50)
-                        .map((num)=>num*10)
-console.log(result)
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = numbers.map((num) =>{return num * 10;})
+//                         .filter((num)=>num<50)
+//                         .map((num)=>num*10)
+// console.log(result)
+
+
+const arr = [1,2,3,5];
+console.log(arr.map((a)=>a))
