ba5b93a2624d165305b4a947b98bb69ce0fd1f8c
swapnil ranmale <83511117+swapnilranmale2003@users.noreply.github.com>
2024-03-15 05:32:14 +0000

    Project 1 solution

---
+++ 08_Project/project.md
@@ -0,0 +1,6 @@
+#project1
+
+```
+console.log("Hello World");
+
+```
---
+++ 08_Project/script.js
@@ -0,0 +1,43 @@
+// const grayClick = () => {
+//     console.log("You clicked on gray button");
+//     document.body.style.backgroundColor = "gray";
+// }
+// const whiteClick = () => {
+//     console.log("You clicked on White button");
+//     document.body.style.backgroundColor = "White";
+// }
+// const blueClick = () => {
+//     console.log("You clicked on blue button");
+//     document.body.style.backgroundColor = "blue";
+// }
+// const yellowClick = () => {
+//     console.log("You clicked on yellow button");
+//     document.body.style.backgroundColor = "Yellow";
+// }
+
+const buttons = document.querySelectorAll('.button');
+// console.log(buttons)
+const body = document.querySelector("body");
+
+buttons.forEach(function (button){
+    button.addEventListener('click',function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id ==='gray')
+        {
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id ==='white')
+        {
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id ==='blue')
+        {
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id ==='yellow')
+        {
+            body.style.backgroundColor=e.target.id
+        }
+    })
+})
